402b22bdb4f93f2750a47a73aa13b1d7463f7355
vgiv <vgivanov@mail.ru>
2019-12-27 11:53:18 +0300

    Updating to 1.3

--- SRC/easypastvu.js
+++ SRC/easypastvu.js
@@ -3,11 +3,12 @@ putcoord = function() {
   let found = document.head.textContent.match(/"geo":\[(.*?)]/);
   if ( found ) {
           let xy = found[1];
+          let xyarr = xy.split(',');
           let elem = document.getElementById('divxy');
           if ( elem == null ) {
             let elems = document.querySelectorAll('div.info');
             if ( elems ) {
-              elems[1].innerHTML = '<div class="info" id="divxy"><span class="infoName">Координаты:</span>'+xy+'</div>\n' + elems[1].innerHTML;
+              elems[1].innerHTML = '<div class="info" id="divxy"><span class="infoName">Координаты:</span>'+xy+'&nbsp;<a href="http://wikimapia.org/#lang=ru&lat='+xyarr[0]+'&lon='+xyarr[1]+'&z=15&m=w&v=1" target="_blank">[W]</a></div>\n' + elems[1].innerHTML;
             }
           } else {
             elem.innerHTML = '<span class="infoName">Старые координаты (обновите страницу!):</span>'+xy
@@ -22,7 +23,7 @@ callback = function(mutationRecords) {
     }
   }
 }
-console.log('EasyPastvu: ver.1.21');
+console.log('EasyPastvu: ver.1.3');
 let observer = new MutationObserver( callback );
 observer.observe( document.body, {subtree: true, attributes: true, characterData: true, attributeFilter: ["download"]} );
 
